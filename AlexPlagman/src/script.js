f9e04b5699117b749e55296b6407f979bf0fbf79
natrastellar <me@natra.dev>
2016-05-01 14:59:48 -0700

    Main Site v1.1

--- AlexPlagman/src/script.js
+++ AlexPlagman/src/script.js
@@ -2,43 +2,66 @@ var $footer = $("footer");
 var $footerBanner = $("#footer_banner");
 var $galleryItems = $(".galleryItem");
 
+var footerRelative = function () {
+    $footer.removeAttr("style");
+    $footer.css({
+        "visibility": "visible",
+        "height": "auto",
+        "padding-top": "1em"
+    });
+    $footerBanner.removeAttr("style");
+    $footerBanner.css({
+        "overflow": "visible",
+        "position": "relative",
+        "bottom": "auto",
+        "height": "auto"
+    });
+    $footerBanner.children().css({
+        "position": "relative",
+        "top": "auto"
+    });
+};
+
+var footerFixed = function (y) {
+    if (y < 0) { y = 0; }
+    var pageHeight = document.body.offsetHeight - window.innerHeight;
+
+    if (pageHeight - y < 220) {
+        $footer.removeAttr("style");
+        $footerBanner.removeAttr("style");
+        $footer.css("visibility", "visible");
+        var h = (200 - (pageHeight - y));
+        if (h < 0) { h = 0; }
+        $footerBanner.css("height", h + "px");
+        $footerBanner.children().css("top", (y - pageHeight) + "px");
+    } else {
+        $("footer").css("visibility", "hidden");
+    }
+};
+
 window.onscroll = function (event) {
     if ($(window).width() <= 600) {
-        $footer.removeAttr("style");
-        $footer.css("visibility", "visible");
-        $footer.css("height", "auto");
-        $footer.css("padding-top", "1em");
-        $footerBanner.removeAttr("style");
-        $footerBanner.css("overflow", "visible");
-        $footerBanner.css("position", "relative");
-        $footerBanner.css("bottom", "auto");
-        $footerBanner.css("height", "auto");
-        $footerBanner.children().css("position", "relative");
+        footerRelative();
 	} else {
-        var y = window.scrollY;
-        if (y < 0) { y = 0; }
-        var pageHeight = document.body.offsetHeight - window.innerHeight;
-
-        if (pageHeight - y < 220) {
-            $footer.removeAttr("style");
-            $footerBanner.removeAttr("style");
-            $footer.css("visibility", "visible");
-            var h = (200 - (pageHeight - y));
-            if (h < 0) { h = 0; }
-            $footerBanner.css("height", h + "px");
-            $footerBanner.children().css("top", (y - pageHeight) + "px");
-        } else {
-            $("footer").css("visibility", "hidden");
-        }
+        footerFixed(window.scrollY);
     }
 };
 
 $galleryItems.on({
     mouseenter: function () {
-        window.onscroll();
+        $(this).addClass("galItemHover");
     },
     mouseleave: function () {
-        window.onscroll();
+        $(this).removeClass("galItemHover");
+    }
+});
+
+$(".skillBar").on({
+    mouseenter: function () {
+        $(".galleryItem[langtag=" + $(this).attr("langtag") + "]").addClass("galItemHover");
+    },
+    mouseleave: function () {
+        $(".galleryItem[langtag=" + $(this).attr("langtag") + "]").removeClass("galItemHover");
     }
 });
 
@@ -47,5 +70,5 @@ $(document).ready(function () {
 });
 
 $(window).resize(function () {
-    window.onscroll(); 
+    window.onscroll();
 });
