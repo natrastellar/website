05e0bf0d2c7297e999cbbf0be15a71f74d01cff2
natrastellar <me@natra.dev>
2019-03-21 21:52:00 -0700

    Fix sshpass

--- begin_deploy.sh
+++ begin_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export SSHPASS=$DEPLOY_PASS
 tar -czf package.tgz dist
 sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 sshpass -e scp deploy.sh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
