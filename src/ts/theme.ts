70915eff7417580bdf1fd138e101f53a77a393b1
natrastellar <me@natra.dev>
2022-11-05 15:25:12 -0700

    feat: add a theme toggle button
    If button is never clicked, still rely on prefers-color-scheme

---
+++ src/ts/theme.ts
@@ -0,0 +1,51 @@
+const DARK_THEME_CLASS = 'dark';
+const LIGHT_THEME_CLASS = 'light';
+
+const THEME_KEY = 'theme';
+const DARK_THEME_VALUE = 'dark';
+const LIGHT_THEME_VALUE = 'light';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  themeToggle?.addEventListener('click', () => {
+    toggleTheme();
+  });
+  if (localStorage.getItem(THEME_KEY) === DARK_THEME_VALUE) {
+    document.body.classList.add(DARK_THEME_CLASS);
+    setTheme(true);
+  }
+  else if (localStorage.getItem(THEME_KEY) === LIGHT_THEME_VALUE) {
+    document.body.classList.add(LIGHT_THEME_CLASS);
+    setTheme(false);
+  }
+});
+
+function toggleTheme() {
+  const theme = localStorage.getItem(THEME_KEY);
+  const dark = theme ? theme === DARK_THEME_VALUE : window.matchMedia("prefers-color-scheme: dark").matches;
+  setTheme(!dark);
+}
+
+function setTheme(dark: boolean) {
+  setElementTheme(document.body, dark);
+  const themedElementTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'button', 'nav', 'label', 'svg', 'header', 'li', 'footer'];
+  for (const name of themedElementTags) {
+    const elements = document.getElementsByTagName(name);
+    for (let i = 0; i < elements.length; ++i) {
+      setElementTheme(elements.item(i)!, dark);
+    }
+  }
+  const themedElementClasses = ['project-card', 'project-tag', 'post-meta', 'update', 'post-link-header', 'footer-main'];
+  for (const name of themedElementClasses) {
+    const elements = document.getElementsByClassName(name);
+    for (let i = 0; i < elements.length; ++i) {
+      setElementTheme(elements.item(i)!, dark);
+    }
+  }
+  localStorage.setItem(THEME_KEY, dark ? DARK_THEME_VALUE : LIGHT_THEME_VALUE);
+}
+
+function setElementTheme(element: Element, dark: boolean) {
+  element.classList.toggle(DARK_THEME_CLASS, dark);
+  element.classList.toggle(LIGHT_THEME_CLASS, !dark);
+}
